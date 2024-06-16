059be0dbfbca19f8c2b9107847ca6d271b5acfd8
tony-tripulca <tony.tripulca@gmail.com>
2024-06-17 01:02:45 +0800

    Clean up routes

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -3,7 +3,7 @@ import express from "express";
 import { app } from "./Server.js";
 
 import Controller from "./controllers/Controller.js";
-import CheckoutController from "./controllers/CheckoutController.js";
+import EmailController from "./controllers/EmailController.js";
 
 const store = express.Router();
 
@@ -11,12 +11,6 @@ const store = express.Router();
  * Store routes
  */
 app.use("/store", store);
-store.get("/orders", CheckoutController.list);
-store.post("/orders", CheckoutController.create);
-store.get("/orders/:order_uid", CheckoutController.read);
-store.put("/orders/:order_uid", CheckoutController.update);
-store.delete("/orders/:order_uid", CheckoutController.delete);
-store.post("/orders/purchase", CheckoutController.purchase);
 
 /**
  * Base routes
